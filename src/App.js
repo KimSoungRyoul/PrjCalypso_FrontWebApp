446069370b93cd900715d6cf1a9391c4b48e503d
KimSoungRyoul <KimSoungRyoul@gmail.com>
2018-05-29 21:39:03 +0900

    Init 프로젝트 세팅

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar"
+import ToolBar from "@material-ui/core/Toolbar"
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar>
+                    sdfsdf
+
+                </AppBar>
+                <ToolBar>asdf</ToolBar>
+                <Button variant="raised" color="primary">
+                    Hello World
+                </Button>
+            </div>
+        );
+    }
+}
+
+export default App;
