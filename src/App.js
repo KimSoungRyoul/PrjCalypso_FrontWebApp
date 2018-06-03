88dcf7d1a37c5360c2a0044fa0c325b4d9d1d3a6
jonghwa <jonghwa0710@naver.com>
2018-06-03 16:09:40 +0900

    react UI Templete
    Semetic UI Free templete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar"
 import ToolBar from "@material-ui/core/Toolbar"
@@ -8,11 +8,6 @@ class App extends Component {
     render() {
         return (
             <div>
-                <AppBar>
-                    sdfsdf
-
-                </AppBar>
-                <ToolBar>asdf</ToolBar>
                 <Button variant="raised" color="primary">
                     Hello World
                 </Button>
