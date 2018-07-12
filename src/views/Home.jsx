01f7b458cac29e112722bf300a6bbf73454d3194
jonghwa <jonghwa0710@naver.com>
2018-07-12 15:51:19 +0900

    add indexpage NewPost  view
    작업더필요 !!

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,39 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-
-import BannerBottom from '../components/index/BannerBottom';
-import Welcome from '../components/index/Welcome';
-import BlogBottem from '../components/index/BlogBottom';
-import NewsLetter from '../components/index/NewsLetter';
-import Footer from '../components/basic/Footer';
-import DialogModal from '../components/basic/DialogModal';
-import SignIn from '../components/basic/SignIn';
+import BannerBottom from "../components/index/BannerBottom";
+import Welcome from "../components/index/Welcome";
+import BlogBottem from "../components/index/BlogBottom";
+import NewsLetter from "../components/index/NewsLetter";
+import Footer from "../components/basic/Footer";
+import DialogModal from "../components/basic/DialogModal";
+import SignIn from "../components/basic/SignIn";
 import Header from "../components/basic/Header";
-
+import NewPost from "../components/index/NewPost";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header>
-                    <Header isHome={true}/>
-                </header>
-                <body>
-                <BannerBottom/>
-                <Welcome/>
-                {/* <Service/>*/}
-                <BlogBottem/>
-                <NewsLetter/>
-                <Footer/>
-
-                </body>
-
-
-                <DialogModal/>
-                <SignIn/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Header isHome={true} />
+        </header>
+        <body>
+          {/* <Service/>*/}
+          <NewPost className="newPoster" />
+          <Footer />
+        </body>
+        <SignIn />
+      </div>
+    );
+  }
 }
 
 export default Home;
